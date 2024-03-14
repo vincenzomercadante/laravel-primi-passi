b7e52a7c782d99dff49bf048b15495f8335c0c4d
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-03-14 16:10:23 +0100

    first commit

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>First Page Laravel</title>
+
+        <!-- import css -->
+        <link rel="stylesheet" href="../css/homepage.css">
+    </head>
+    <body>
+        <h1>PRIMA PAGINA CON LARAVEL</h1>
+    </body>
+</html>
